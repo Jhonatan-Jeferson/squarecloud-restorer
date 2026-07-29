794213f783a1a4dbb4d15f6d8240c8c871a066df
Jhonatan Jeferson <jhonatanjefersonoliveira@gmail.com>
2026-07-29 11:50:16 -0300

    refactor: rewrite list command since new http implementation.

--- src/squarecloud_restorer/commands.py
+++ src/squarecloud_restorer/commands.py
@@ -1,9 +1,10 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, List
 from datetime import datetime
 from queue import Queue
 from getpass import getpass
-from .utils import get_api_key
-from .core import BaseSnapshot, Http
+
+from .core import URL, HTTPRequest, ApplicationSnapshot, BaseSnapshot, DatabaseSnapshot
+from .utils import get_api_key, sort_snapshots
 
 if TYPE_CHECKING:
     from .__main__ import RestorerClient
@@ -18,24 +19,46 @@ def help(client: RestorerClient) -> None:
 
 @get_api_key
 def list(client: RestorerClient) -> None:
-    """List all Snapshots ordering by most recent for restore."""
-    _http = Http()
-    _http.set_api_key(client.api_key)
-    all_snapshots = _http.get_snapshots(True, True)
+    "List all Snapshots ordering by most recent for restore."
+    apps_endpoint = URL.get_snapshot_list('applications')
+    databases_endpoint = URL.get_snapshot_list('databases')
+    user_endpoint = URL.user()
+    HEADERS = {
+        'Content-Type': 'application/json',
+        'Authorization': client.api_key
+    }
+    user_request = HTTPRequest(user_endpoint, HEADERS, {}, 'GET')
+    user_response = user_request.request()
+    if user_response.status_code != 200: raise Exception('Cannot get user data.')
+    user_response.read()
+    account_id = user_response.json["response"]["user"]["id"]
+    requests = HTTPRequest(apps_endpoint, HEADERS, {}, 'GET'), \
+        HTTPRequest(databases_endpoint, HEADERS, {}, 'GET')
+    responses = tuple([request.request() for request in requests])
+    all_snapshots: dict[str, List[BaseSnapshot]] = {'apps': [], 'dbs': []}
+    for index, response in enumerate(responses): 
+        if response.status_code != 200: raise Exception('Requests do not received a correct response.')
+        response.read()
+        list_of_snapshots: List[dict[str, Any]] = response.json["response"]
+        snapshots = sort_snapshots(list_of_snapshots)
+        filtered_snapshots = {item['name']: item for item in snapshots}
+        sorted_snapshots = sort_snapshots([snap for snap in filtered_snapshots.values()], reverse=True)
+        if index == 0: all_snapshots['apps'].extend([ApplicationSnapshot(**snap_data, account_id=account_id) for snap_data in sorted_snapshots])
+        else: all_snapshots['dbs'].extend([DatabaseSnapshot(**snap_data, account_id=account_id) for snap_data in sorted_snapshots])
     queue: Queue[BaseSnapshot] = Queue()
     for k,v in all_snapshots.items():
-        print(f"{k:^23} - {'date(dd/mm/yyyy)':^20}")
+        print(f"{k:^32} - {'date(dd/mm/yyyy)':^20}")
         for app in v:
             print(f"{app.name:<} - {datetime.fromisoformat(app.modified).strftime("%d/%m/%Y")} ")
             queue.put(app)
-    while not queue.empty():
-        snapshot = queue.get()
-        restore = input(f'Restore {snapshot.name}({datetime.fromisoformat(snapshot.modified).strftime("%d/%m/%Y")})?(y/n)\n>')
-        match restore.lower():
-            case 'y': snapshot.restore(client.api_key)
-            case 'n': ...
-            case _: ...
-        queue.task_done()
+    # while not queue.empty():
+    #     snapshot = queue.get()
+    #     restore = input(f'Restore {snapshot.name}({datetime.fromisoformat(snapshot.modified).strftime("%d/%m/%Y")})?(y/n)\n>')
+    #     match restore.lower():
+    #         case 'y': snapshot.restore(client.api_key)
+    #         case 'n': ...
+    #         case _: ...
+    #     queue.task_done()
     
 
 @get_api_key
@@ -50,5 +73,5 @@ def applications(client: RestorerClient) -> None:
 
 @get_api_key
 def databases(client: RestorerClient) -> None:
-    """list only databases Snapshots for restore."""
+    "list only databases Snapshots for restore."
     ...
