1316188810d3fd59ce45c42350a452c25635c41a
Jhonatan Jeferson <jhonatanjefersonoliveira@gmail.com>
2026-07-30 13:43:45 -0300

    chore: rename api_key decorator

--- src/squarecloud_restorer/utils.py
+++ src/squarecloud_restorer/utils.py
@@ -1,16 +1,18 @@
-from typing import Any, ParamSpec, TypeVar, cast
+from typing import Any, ParamSpec, TypeVar, cast, TYPE_CHECKING
 from collections.abc import Callable
 from functools import wraps
-from .__main__ import RestorerClient
 from getpass import getpass
+
+if TYPE_CHECKING:
+    from .__main__ import RestorerClient
 
 P = ParamSpec("P")
 R = TypeVar("R")
 
-def get_api_key(f: Callable[P, R]) -> Callable[P, R]:
+def require_api_key(f: Callable[P, R]) -> Callable[P, R]:
     @wraps(f)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-        client: RestorerClient = cast(RestorerClient, args[0])
+        client = cast("RestorerClient", args[0])
         client.api_key = getpass("Insert Square Cloud API Key: ", echo_char="*")
         return f(*args, **kwargs)
 
